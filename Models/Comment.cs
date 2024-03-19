45d455edfe9b5eaf5b4f73e04b48c047c0a9267b
Iagoccarvalho <iagocarvalho_@outlook.com>
2024-03-19 19:07:20 -0300

    Criação das entidades Stock e Comment

---
+++ Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Api_NET8.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now.ToUniversalTime();
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; } //Propriedade de navegação 
+        }
+}
